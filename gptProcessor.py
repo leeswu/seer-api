69ae39030e3b2a426c8acda27ebefda99de00f39
Sophie Wu <swu.lee02@gmail.com>
2025-03-13 12:13:55 -0700

    add back in alt text injection example

--- gptProcessor.py
+++ gptProcessor.py
@@ -5,6 +5,7 @@ from os import path
 import re
 import json
 
+
 class GPTProcessor:
     def __init__(self, api_key):
         self.client = OpenAI(api_key=api_key)
@@ -13,8 +14,8 @@ class GPTProcessor:
         self.base_dir = "processed"
         self.images_path = path.join(self.base_dir, "images")
 
-
     # Get each pages as a base64 encoded images
+
     def get_pages(self, file_path):
         print("Starting page extraction...")
         doc = pymupdf.open(file_path)
@@ -29,7 +30,6 @@ class GPTProcessor:
             base64_pages.append(img_base64)
         print("Page extraction complete.")
         return base64_pages
-    
 
     def get_alt_text(self, pages):
         print("Starting alt text generation...")
@@ -39,7 +39,7 @@ class GPTProcessor:
         You will be given a list of images that represent sequential pages of a document.
         Your task is to generate a detailed alt text description for each image in those pages.
         Base your output on the requirements and the example below.
-        
+
         Requirements:
         1. You output should consist of a list of new line separated JSON objects, structured as follows: {"imageName": <IMAGE_NAME>, "altText": <ALT_TEXT>}
         where the name of the image or figure should go where <IMAGE_NAME> is and the alt text should go where <ALT_TEXT> is.
@@ -50,24 +50,22 @@ class GPTProcessor:
         Example:
         {"imageName": "Figure 1.", "altText": "This is the alt text for figure 1"}\n
         {"imageName": "Figure 8.", "altText": "This is some alt text for figure 8"}\n
-        {"imageName": "Unlabeled Figure.", "altText": "This is alt text for an unlabeled figure."}
+        {"imageName": "Unlabeled Figure.",
+            "altText": "This is alt text for an unlabeled figure."}
         """
 
         USER_PROMPT = """
         Generate highly detailed and descriptive alt text for all the images in this page.
-        
+
         Return a list of new line separated JSON objects that contain alt text for each image, structured as follows: {"imageName": <IMAGE_NAME>, "altText": <ALT_TEXT>}
         where the name of the image or figure should go where <IMAGE_NAME> is and the alt text should go where <ALT_TEXT> is.
         Return no other text than the JSON objects.
         """
 
-
         try:
             alt_texts = []
 
             for i, page in enumerate(pages):
-
-
 
                 response = self.client.chat.completions.create(
                     model=self.model,
@@ -82,7 +80,8 @@ class GPTProcessor:
                 alt_text = response.choices[0].message.content.split("\n")
                 print("ALT TEXT AFTER SPLIT:")
                 print(alt_text)
-                alt_text = [json.loads(alt) for alt in alt_text if self._is_json(alt)]
+                alt_text = [json.loads(alt)
+                                       for alt in alt_text if self._is_json(alt)]
 
                 for alt in alt_text:
                     alt["altText"] = "AI-Generated Alt Text: " + alt["altText"]
@@ -92,23 +91,23 @@ class GPTProcessor:
             print(f"Error generating transcript: {e}")
             return None
         return alt_texts
-    
+
     def _is_json(self, myjson):
         try:
             json.loads(myjson)
         except ValueError as e:
             return False
         return True
-    
+
     def get_structured_md_incremental(self, pages):
         print("Starting incremental processing...")
         structured_pages = []
-        
+
         # Get alt text and transcription for each page
         # alt_texts = self.get_alt_text(pages)
         # if not alt_texts:
         #     return None
-            
+
         # page_transcripts = self.get_raw_transcription(pages)
         # if not page_transcripts:
         #     return None
@@ -119,24 +118,52 @@ class GPTProcessor:
 
             page_alt_text = self.get_alt_text([page])
             page_transcript = self.get_raw_transcription([page])
-            
+
             SYSTEM_PROMPT = """
-            Given a markdown transcript and a list of alt text descriptions for images, change heading and subheading levels as needed and inject the alt text descriptions into the right places to return a properly formatted, logically structured markdown document.
-            
+            You are a helpful assistant that carefully and properly reconstructs markdown transcripts.
+
+            Given a markdown transcript and a list of alt text descriptions for images in the form of JSON objects, reconstruct the the raw markdown
+            transcript by injecting the alt text into the appropriate places. Ensure that you output a properly formatted, logically structured markdown document.
+
+            Base your output on the requirements and example input and example output below.
+
             Requirements:
-            1. Convert any actual references to actual images, including links, brackets, filepaths, or source information in the markdown to plain markdown.
-            2. Leave all original text and captions intact and in the same place they were in the original markdown.
-            3. Remove any and all markdown code blocks.
-            4. Return no other text than the markdown.
+            1. Remove any and all references to actual image content, including links, brackets, filepaths, or source information in the markdown to plain markdown.
+            2. Leave all original text and captions intact and in the same place they were in the raw transcript.
+            3. Inject alt text for the image immediately after the existing image caption.
+            4. Remove any and all markdown code blocks.
+            5. Return no other text than the markdown.
+
+            Example Input:
+
+            Alt Text: [{"figureName": "Figure 1", "altText": "AI-Generated Alt Text: This is alt text for figure 1."}]
+
+            Raw Transcript:
+
+            # Main Title of the Document
+
+            Here is some main body text.
+
+            ### Figure 1. A caption for figure 1.
+
+            Example Output:
+
+            # Main Title of the Document
+
+            Here is some main body text.
+
+            ### Figure 1. A caption for figure 1.
+            AI-Generated Alt Text: This is alt text for figure 1.
             """
 
             USER_PROMPT = f"""
-            Given a markdown transcript and a list of alt text descriptions for images, reformat headings and subheadings as needed and inject the alt text descriptions into the right places to return a properly formatted, logically structured markdown document.
-            
-            Here is the list of alt text descriptions for images:
+            Given a markdown transcript and a list of alt text descriptions for images in the form of JSON objects, reconstruct the the raw markdown
+            transcript by injecting the alt text into the appropriate places. Ensure that you output a properly formatted, logically structured markdown document.
+
+            Alt Text:
             {page_alt_text}
 
-            Here is the markdown transcript:
+            Raw Transcript:
             {page_transcript}
             """
 
@@ -148,7 +175,8 @@ class GPTProcessor:
                         {"role": "user", "content": USER_PROMPT}
                     ]
                 )
-                structured_page = response.choices[0].message.content.replace("```md", "").replace("```", "").replace("markdown", "")
+                structured_page = response.choices[0].message.content.replace(
+                    "```md", "").replace("```", "").replace("markdown", "")
                 structured_page = re.sub(r"!\[(.*?)\]", r"\1", structured_page)
 
                 structured_pages.append(structured_page)
